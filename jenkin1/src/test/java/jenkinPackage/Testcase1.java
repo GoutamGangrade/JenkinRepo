e66ceae80c417bd69afd8547e3c87dd52b97c238
GoutamGangrade <thisisgoutam1400@gmail.com>
2023-09-08 10:41:30 +0530

    commit

--- jenkin1/src/test/java/jenkinPackage/Testcase1.java
+++ jenkin1/src/test/java/jenkinPackage/Testcase1.java
@@ -1,9 +1,53 @@
 package jenkinPackage;
+import java.time.Duration;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class Testcase1 {
-  @Test
-  public void f() {
+	
+	
+  @Test(description = "Check Login Page is loaded Successfully")
+  public void TC1() {
+	  
+	  WebDriver driver = new ChromeDriver();
+	  driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+
+      // Enter username and password
+      driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+      driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[1]/div/div[2]/input")).sendKeys("Admin");
+      driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[2]/div/div[2]/input")).sendKeys("admin123");
+      driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button")).click();
+
+      System.out.println("TestCase1: Login Page is loaded Successfully");
+  
+  }
+  @Test(description = "Check Logout is working fine", dependsOnMethods = "TC1")
+  public void TC2() {
+	  
+	  
+	  WebDriver driver = new ChromeDriver();
+	  driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+
+      // Enter username and password
+      driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+      driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[1]/div/div[2]/input")).sendKeys("Admin");
+      driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[2]/div/div[2]/input")).sendKeys("admin123");
+      driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button")).click();
+
+	  
+	  // Wait for profile icon and click on it
+      driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+      driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div[1]/header/div[1]/div[2]/ul")).click();
+
+      // Wait for logout link and click on it
+      driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div[1]/header/div[1]/div[2]/ul/li/ul/li[4]/a")).click();
+		System.out.println("sucessfully log out");
+
+      // Close the browser
+      //driver.quit();
+	  
   }
 }
